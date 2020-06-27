3a8e99765c84b6aa9a18b2891ee61d5ccf7a6875
Sutama Mitra <sutama.mitra@outlook.com>
2020-06-27 10:11:41 -0400

    First ckeckin from Sutama

---
+++ In-classHtmlLinks/Models/input.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace In_classHtmlLinks.Models
+{
+    public class input
+    {
+        public string name { get; set; }
+        public string state { get; set; }
+
+    }
+}
